b35f47567b4896cf811bd9071458910b30f4ed4d
CoffeeCatRailway <dragonlee1002@gmail.com>
2021-09-24 10:09:16 +1000

    :fire: Removed redundant mixins

--- src/main/java/io/github/coffeecatrailway/plus/mixin/FrostWalkerEnchantmentMixin.java
+++ src/main/java/io/github/coffeecatrailway/plus/mixin/FrostWalkerEnchantmentMixin.java
@@ -1,14 +1,10 @@
 package io.github.coffeecatrailway.plus.mixin;
 
 import io.github.coffeecatrailway.plus.PlusConfig;
-import io.github.coffeecatrailway.plus.registry.PlusEnchantments;
-import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.FrostWalkerEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * @author CoffeeCatRailway
@@ -21,11 +17,5 @@ public class FrostWalkerEnchantmentMixin
     private static int onEntityMoved(int b)
     {
         return (int) (b - 2 + PlusConfig.SERVER.frostWalkerLevel.get());
-    }
-
-    @Inject(method = "checkCompatibility(Lnet/minecraft/world/item/enchantment/Enchantment;)Z", at = @At("RETURN"), cancellable = true)
-    public void checkCompatibility(Enchantment enchantment, CallbackInfoReturnable<Boolean> callback)
-    {
-        callback.setReturnValue(callback.getReturnValue() && enchantment != PlusEnchantments.HEAT_WALKER.get());
     }
 }
