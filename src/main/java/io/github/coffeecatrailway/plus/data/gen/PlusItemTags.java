9802fa48508a1e3a0815852a93d5d19b6193f893
CoffeeCatRailway <dragonlee1002@gmail.com>
2021-09-26 11:49:24 +1000

    :fire: Removed basalt item tag

--- src/main/java/io/github/coffeecatrailway/plus/data/gen/PlusItemTags.java
+++ src/main/java/io/github/coffeecatrailway/plus/data/gen/PlusItemTags.java
@@ -1,15 +1,9 @@
 package io.github.coffeecatrailway.plus.data.gen;
 
 import io.github.coffeecatrailway.plus.PlusMod;
-import io.github.coffeecatrailway.plus.registry.PlusBlocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.ItemTags;
-import net.minecraft.tags.Tag;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 import javax.annotation.Nullable;
@@ -21,8 +15,6 @@ import javax.annotation.Nullable;
 @SuppressWarnings("unchecked")
 public class PlusItemTags extends ItemTagsProvider
 {
-    public static final Tag.Named<Item> BASALT = ItemTags.createOptional(new ResourceLocation("forge", "basalt"));
-
     public PlusItemTags(DataGenerator generator, BlockTagsProvider provider, @Nullable ExistingFileHelper fileHelper)
     {
         super(generator, provider, PlusMod.MOD_ID, fileHelper);
@@ -31,6 +23,5 @@ public class PlusItemTags extends ItemTagsProvider
     @Override
     protected void addTags()
     {
-        this.tag(BASALT).add(Blocks.BASALT.asItem(), PlusBlocks.BRITTLE_BASALT.get().asItem());
     }
 }
