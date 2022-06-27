f64c1aa5641804bbaac6f18a88ff4c782c70ee2a
CoffeeCatRailway <dragonlee1002@gmail.com>
2022-06-27 20:35:52 +1000

    :fire: Removed `PlusItemModels`

--- fabric/src/main/java/io/github/coffeecatrailway/plus/fabric/PlusFabricClient.java
+++ fabric/src/main/java/io/github/coffeecatrailway/plus/fabric/PlusFabricClient.java
@@ -2,17 +2,13 @@ package io.github.coffeecatrailway.plus.fabric;
 
 import dev.emi.trinkets.api.client.TrinketRenderer;
 import dev.emi.trinkets.api.client.TrinketRendererRegistry;
-import io.github.coffeecatrailway.plus.client.entity.AmuletModel;
 import io.github.coffeecatrailway.plus.client.entity.FoxHatModel;
 import io.github.coffeecatrailway.plus.common.item.FoxHatItem;
-import io.github.coffeecatrailway.plus.common.item.WarmthAmuletItem;
 import io.github.coffeecatrailway.plus.registry.PlusItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
-import net.minecraft.client.Minecraft;
 
 /**
  * @author CoffeeCatRailway
@@ -24,7 +20,6 @@ public class PlusFabricClient implements ClientModInitializer
     @Override
     public void onInitializeClient()
     {
-        EntityModelLayerRegistry.registerModelLayer(FoxHatItem.LAYER, FoxHatModel::createBodyLayer);
         ArmorRenderer.register((matrices, vertexConsumers, stack, entity, slot, light, contextModel) -> {
             FoxHatModel model = FoxHatItem.getModel();
             contextModel.copyPropertiesTo(model);
@@ -35,7 +30,6 @@ public class PlusFabricClient implements ClientModInitializer
                 ArmorRenderer.renderPart(matrices, vertexConsumers, light, stack, model, FoxHatItem.SNOW_TEXTURE);
         }, PlusItems.FOX_HAT.get(), PlusItems.SNOW_FOX_HAT.get());
 
-        EntityModelLayerRegistry.registerModelLayer(WarmthAmuletItem.LAYER, AmuletModel::createBodyLayer);
         TrinketRendererRegistry.registerRenderer(PlusItems.WARMTH_AMULET.get(), (TrinketRenderer) PlusItems.WARMTH_AMULET.get());
     }
 }
