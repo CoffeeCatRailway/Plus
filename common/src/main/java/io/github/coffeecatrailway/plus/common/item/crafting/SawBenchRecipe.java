fe5d4802dcd0b1fa2e721412be341e6dcba09af9
CoffeeCatRailway <dragonlee1002@gmail.com>
2022-08-24 13:14:38 +1000

    Revert ":bug: Fix fabric crash"
    This reverts commit 454da61e1a9961d22cac3fa327085cd522b6f5f8.

--- common/src/main/java/io/github/coffeecatrailway/plus/common/item/crafting/SawBenchRecipe.java
+++ common/src/main/java/io/github/coffeecatrailway/plus/common/item/crafting/SawBenchRecipe.java
@@ -1,16 +1,11 @@
 package io.github.coffeecatrailway.plus.common.item.crafting;
 
-import com.google.gson.JsonObject;
 import io.github.coffeecatrailway.plus.registry.PlusBlocks;
 import io.github.coffeecatrailway.plus.registry.PlusRecipes;
-import net.minecraft.core.Registry;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.GsonHelper;
 import net.minecraft.world.Container;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.SingleItemRecipe;
 import net.minecraft.world.level.Level;
 
@@ -35,50 +30,5 @@ public class SawBenchRecipe extends SingleItemRecipe
     public ItemStack getToastSymbol()
     {
         return new ItemStack(PlusBlocks.SAW_BENCH.get());
-    }
-
-    public static class Serializer implements RecipeSerializer<SawBenchRecipe>
-    {
-        final SingleItemMaker factory;
-
-        public Serializer(SingleItemMaker singleItemMaker)
-        {
-            this.factory = singleItemMaker;
-        }
-
-        public SawBenchRecipe fromJson(ResourceLocation location, JsonObject jsonObject)
-        {
-            String string = GsonHelper.getAsString(jsonObject, "group", "");
-            Ingredient ingredient;
-            if (GsonHelper.isArrayNode(jsonObject, "ingredient"))
-                ingredient = Ingredient.fromJson(GsonHelper.getAsJsonArray(jsonObject, "ingredient"));
-            else
-                ingredient = Ingredient.fromJson(GsonHelper.getAsJsonObject(jsonObject, "ingredient"));
-
-            String string2 = GsonHelper.getAsString(jsonObject, "result");
-            int i = GsonHelper.getAsInt(jsonObject, "count");
-            ItemStack itemStack = new ItemStack(Registry.ITEM.get(new ResourceLocation(string2)), i);
-            return this.factory.create(location, string, ingredient, itemStack);
-        }
-
-        public SawBenchRecipe fromNetwork(ResourceLocation location, FriendlyByteBuf buffer)
-        {
-            String string = buffer.readUtf();
-            Ingredient ingredient = Ingredient.fromNetwork(buffer);
-            ItemStack itemStack = buffer.readItem();
-            return this.factory.create(location, string, ingredient, itemStack);
-        }
-
-        public void toNetwork(FriendlyByteBuf buffer, SawBenchRecipe recipe)
-        {
-            buffer.writeUtf(recipe.getGroup());
-            recipe.getIngredients().get(0).toNetwork(buffer);
-            buffer.writeItem(recipe.getResultItem());
-        }
-
-        public interface SingleItemMaker
-        {
-            SawBenchRecipe create(ResourceLocation resourceLocation, String string, Ingredient ingredient, ItemStack itemStack);
-        }
     }
 }
